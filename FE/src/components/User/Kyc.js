5307e76db298f0acb4157517086ac7dc3545f668
ahmar7 <ahmarjabbar7@gmail.com>
2024-03-25 16:55:52 +0500

    kyc

--- FE/src/components/User/Kyc.js
+++ FE/src/components/User/Kyc.js
@@ -1,4 +1,4 @@
-import logo_300x57_1 from "../../assets/img/logo-300x57-1.png";
+import logo_300x57_1 from "../../assets/img/Logo.png";
 import log from "../../assets/img/log.jpg";
 import React, { useEffect, useState } from "react";
 import SideBar from "../../layout/UserSidebar/SideBar";
