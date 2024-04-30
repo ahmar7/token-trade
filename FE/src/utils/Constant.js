19868ed7f8c2e33d7915a1f71ea9747c49356892
ahmar7 <ahmarjabbar7@gmail.com>
2024-05-01 01:11:46 +0500

    br

--- FE/src/utils/Constant.js
+++ FE/src/utils/Constant.js
@@ -3,6 +3,7 @@ module.exports = {
   // baseUrl: "http://localhost:4000/api/v1",
   // Local server
   // Official server
-  baseUrl: "https://token-trade.onrender.com/api/v1",
+  // baseUrl: "https://token-trade.onrender.com/api/v1",
+  baseUrl: "https://tokentrade-server.onrender.com/api/v1",
   // Official server
 };
